57a91364bab88abea08708825568172e03674789
antoniongola <antoniongola10@gmail.com>
2024-05-23 20:58:40 +0100

    almost done

--- src/main/java/com/ngolajr/cashfinderapifinal/CashFinderApiFinalApplication.java
+++ src/main/java/com/ngolajr/cashfinderapifinal/CashFinderApiFinalApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CashFinderApiFinalApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CashFinderApiFinalApplication.class, args);
     }
-
 }
