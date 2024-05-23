2ed4112d7d30f8b5a239fba7faa5abb24dbdf8e7
antoniongola <antoniongola10@gmail.com>
2024-05-23 18:29:00 +0100

    initial commit

---
+++ src/main/java/com/ngolajr/cashfinderapifinal/CashFinderApiFinalApplication.java
@@ -0,0 +1,13 @@
+package com.ngolajr.cashfinderapifinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFinderApiFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashFinderApiFinalApplication.class, args);
+    }
+
+}
